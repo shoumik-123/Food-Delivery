1b77be0b4f0dd8f14eeb86158ab89f9d4a651e5e
shoumik-123 <shabikyeaminshoumik123@gmail.com>
2023-04-27 08:42:56 +0600

    message

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -9,9 +9,9 @@ const Card = () => {
                     <img src="https://www.tbsnews.net/sites/default/files/styles/big_2/public/images/2021/03/19/biriyani.jpg" className="card-img-top" alt="..."/>
                     <div className="card-body">
                         <h5 className="card-title">Card title</h5>
-                        <p className="card-text"></p>
+                        <p className="card-text">About food</p>
                         <div className="container w-100">
-                            <select className="m-2 p-1 h-100 bg-dark text-light rounded">
+                            <select className=" p-1 h-100 bg-dark text-light rounded">
                                 {Array.from(Array(6),(e,i)=>{
                                     return (
                                         <option key={i+1} value={i+1}>
--- client/src/components/Carousel.js
+++ client/src/components/Carousel.js
@@ -3,52 +3,114 @@ import React from 'react';
 const Carousel = () => {
     return (
         <div>
+            {/*<div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">*/}
+            {/*    <div className="carousel-indicators">*/}
+            {/*        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0"*/}
+            {/*                className="active" aria-current="true" aria-label="Slide 1"></button>*/}
+            {/*        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"*/}
+            {/*                aria-label="Slide 2"></button>*/}
+            {/*        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"*/}
+            {/*                aria-label="Slide 3"></button>*/}
+            {/*    </div>*/}
+            {/*    <div className="carousel-inner">*/}
+            {/*        <div className="carousel-item active">*/}
+            {/*            <img src="..." className="d-block w-100" alt="..."/>*/}
+            {/*                <div className="carousel-caption d-none d-md-block">*/}
+            {/*                    <h5>First slide label</h5>*/}
+            {/*                    <p>Some representative placeholder content for the first slide.</p>*/}
+            {/*                </div>*/}
+            {/*        </div>*/}
+            {/*        <div className="carousel-item">*/}
+            {/*            <img src="..." className="d-block w-100" alt="..."/>*/}
+            {/*                <div className="carousel-caption d-none d-md-block">*/}
+            {/*                    <h5>Second slide label</h5>*/}
+            {/*                    <p>Some representative placeholder content for the second slide.</p>*/}
+            {/*                </div>*/}
+            {/*        </div>*/}
+            {/*        <div className="carousel-item">*/}
+            {/*            <img src="..." className="d-block w-100" alt="..."/>*/}
+            {/*                <div className="carousel-caption d-none d-md-block">*/}
+            {/*                    <h5>Third slide label</h5>*/}
+            {/*                    <p>Some representative placeholder content for the third slide.</p>*/}
+            {/*                </div>*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"*/}
+            {/*            data-bs-slide="prev">*/}
+            {/*        <span className="carousel-control-prev-icon" aria-hidden="true"></span>*/}
+            {/*        <span className="visually-hidden">Previous</span>*/}
+            {/*    </button>*/}
+            {/*    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"*/}
+            {/*            data-bs-slide="next">*/}
+            {/*        <span className="carousel-control-next-icon" aria-hidden="true"></span>*/}
+            {/*        <span className="visually-hidden">Next</span>*/}
+            {/*    </button>*/}
+            {/*</div>*/}
 
 
             <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
-                <div className="row">
-                    <div className="col-md-0"></div>
-                    <div className="col-md-12">
-
-                        <div className="carousel-inner">
-
-                            {/*<div> style={{"zIndex":"1"}}*/}
-                            {/*    <form className="d-flex">*/}
-                            {/*        <input className="form-control me-2 text-light" type="search" placeholder="Search"*/}
-                            {/*               aria-label="Search"/>*/}
-                            {/*        <button className="btn btn-outline-success" type="submit">Search</button>*/}
-                            {/*    </form>*/}
-
-                            {/*</div>*/}
 
 
-                            <div className="carousel-item active">
-                                <img src="https://source.unsplash.com/random/300×300/?pastry" className="d-block w-100" alt="..." style={{"height":"700px","marginTop":"-100px"}}/>
-                            </div>
-                            <div className="carousel-item">
-                                <img src="https://source.unsplash.com/random/900×700/?pizza" className="d-block w-100" alt="..." style={{"height":"700px","marginTop":"-100px"}}/>
-                            </div>
-                            <div className="carousel-item">
-                                <img src="https://source.unsplash.com/random/900×700/?burger" className="d-block w-100" alt="..." style={{"height":"700px","marginTop":"-100px"}}/>
+                <div className="carousel-inner">
+
+
+
+
+                    <div className="carousel-item active">
+                        <img src="https://source.unsplash.com/random/300×300/?pastry" className="d-block w-100" alt="..." style={{"height":"700px","marginTop":"-100px","filter":"brightness(30%)"}}/>
+                        <div className="carousel-caption d-none d-md-block">
+                            <div style={{"zIndex":"100"}}>
+                                <form className="d-flex">
+                                    <input className="form-control me-3 text-light " type="search" placeholder="Search"
+                                           aria-label="Search"/>
+                                    <br/>
+                                    <button className="btn btn-success" type="submit">Search</button>
+                                </form>
+
                             </div>
                         </div>
-
-
-                        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
-                                data-bs-slide="prev">
-                            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span className="visually-hidden">Previous</span>
-                        </button>
-                        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
-                                data-bs-slide="next">
-                            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span className="visually-hidden">Next</span>
-                        </button>
-
                     </div>
-                    <div className="col-md-0"></div>
+                    <div className="carousel-item">
+                        <img src="https://source.unsplash.com/random/900×700/?pizza" className="d-block w-100" alt="..." style={{"height":"700px","marginTop":"-100px","filter":"brightness(30%)"}}/>
+                        <div className="carousel-caption d-none d-md-block">
+                            <div style={{"zIndex":"100"}}>
+                                <form className="d-flex">
+                                    <input className="form-control me-3 text-light " type="search" placeholder="Search"
+                                           aria-label="Search"/>
+                                    <br/>
+                                    <button className="btn btn-success" type="submit">Search</button>
+                                </form>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div className="carousel-item">
+                        <img src="https://source.unsplash.com/random/900×700/?burger" className="d-block w-100" alt="..." style={{"height":"700px","marginTop":"-100px","filter":"brightness(30%)"}}/>
+                        <div className="carousel-caption d-none d-md-block">
+                            <div style={{"zIndex":"100"}}>
+                                <form className="d-flex">
+                                    <input className="form-control me-3 text-light " type="search" placeholder="Search"
+                                           aria-label="Search"/>
+                                    <br/>
+                                    <button className="btn btn-success" type="submit">Search</button>
+                                </form>
+
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
+
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
+                        data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
+                        data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
             </div>
         </div>
     );
